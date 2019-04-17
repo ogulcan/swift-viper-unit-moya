5b876a2181895965480a33afe57713521afc55af
Ogulcan <junkchorn@gmail.com>
2019-04-18 01:39:41 +0300

    Initial commit

---
+++ ViperApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperApp
+//
+//  Created by Oğulcan on 18.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
