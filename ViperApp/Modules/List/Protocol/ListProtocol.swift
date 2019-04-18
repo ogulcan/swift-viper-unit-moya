1c869777290339c7dd45800dac3f62b08c809915
Ogulcan <junkchorn@gmail.com>
2019-04-18 15:12:13 +0300

    Boilerplate

---
+++ ViperApp/Modules/List/Protocol/ListProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ListProtocol.swift
+//  ViperApp
+//
+//  Created by Oğulcan on 18.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import Foundation
---
+++ ViperApp/Modules/List/Router/ListRouter.swift
@@ -0,0 +1,9 @@
+//
+//  ListRouter.swift
+//  ViperApp
+//
+//  Created by Oğulcan on 18.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import Foundation
