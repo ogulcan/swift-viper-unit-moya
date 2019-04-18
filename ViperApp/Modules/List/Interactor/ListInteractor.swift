1be453a123eef5560b92ff915491814c067ab6b9
Ogulcan <junkchorn@gmail.com>
2019-04-18 22:48:54 +0300

    ListInteractor

---
+++ ViperApp/Modules/List/Interactor/ListInteractor.swift
@@ -0,0 +1,19 @@
+//
+//  ListInteractor.swift
+//  ViperApp
+//
+//  Created by Oğulcan on 18.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListInteractor: PresenterToInteractorProtocol {
+    
+    var presenter: InteractorToPresenterProtocol?
+    
+    func retrieveList() {
+        
+    }
+}g'
