38f59905d783ece3698ed7ce53c8909701e1e7ad
Ogulcan <junkchorn@gmail.com>
2019-04-18 17:09:27 +0300

    ListViewController

---
+++ ViperApp/Modules/List/View/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  ViperApp
+//
+//  Created by Oğulcan on 18.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    var presenter: ViewToPresenterProtocol?
+    
+    @IBOutlet weak var tableView: UITableView!
+
+}
