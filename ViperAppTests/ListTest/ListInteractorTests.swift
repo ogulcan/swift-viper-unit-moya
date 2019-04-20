ce8f8b03ea90e55c891ec95f0d3f10ace6539044
Ogulcan <junkchorn@gmail.com>
2019-04-20 21:00:28 +0300

    Interactor tests

---
+++ ViperAppTests/ListTest/ListInteractorTests.swift
@@ -0,0 +1,40 @@
+//
+//  ListInteractorTests.swift
+//  ViperAppTests
+//
+//  Created by Oğulcan on 20.04.2019.
+//  Copyright © 2019 Ogulcan. All rights reserved.
+//
+
+import XCTest
+import Moya
+@testable import ViperApp
+
+class ListInteractorTests: XCTestCase {
+
+    var provider: MoyaProvider<TMDbApi>!
+
+    override func setUp() {
+        super.setUp()
+        provider = MoyaProvider<TMDbApi>(stubClosure: MoyaProvider.immediatelyStub)
+    }
+    
+    func testRetrieveList() {
+        provider.request(.listActors()) { result in
+            if case let .success(response) = result {
+                do {
+                    XCTAssertNoThrow(try response.filterSuccessfulStatusCodes())
+                    XCTAssertNoThrow(try response.map(BaseResponse<[Actor]>.self))
+                    
+                    let baseResponse = try response.map(BaseResponse<[Actor]>.self)
+                    XCTAssertEqual(baseResponse.page, 1)
+                    XCTAssertGreaterThan(baseResponse.page, 0)
+                    XCTAssertGreaterThan(baseResponse.total_pages, 0)
+                    XCTAssertGreaterThan(baseResponse.results.count, 0)
+                } catch let error {
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+}
